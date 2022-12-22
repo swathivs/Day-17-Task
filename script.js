6ae9fe6f7437c3a9b854f888404fda7a01b4493a
swathivs <36623544+swathivs@users.noreply.github.com>
2022-12-22 17:19:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+    let n=10;
+    
+    setTimeout(()=>{
+        txt.innerHTML=n--;
+        setTimeout(()=>{
+            txt.innerHTML=n--;
+            setTimeout(()=>{
+                txt.innerHTML=n--;
+                setTimeout(()=>{
+                    txt.innerHTML=n--;
+                    setTimeout(()=>{
+                        txt.innerHTML=n--;
+                        setTimeout(()=>{
+                            txt.innerHTML=n--;
+                            setTimeout(()=>{
+                                txt.innerHTML=n--;
+                                setTimeout(()=>{
+                                    txt.innerHTML=n--;
+                                    setTimeout(()=>{
+                                        txt.innerHTML=n--;
+                                        setTimeout(()=>{
+                                            txt.innerHTML=n--;
+                                            setTimeout(()=>{
+                                                txt.innerHTML="";
+                                                txt.style.visibility="hidden";
+                                                msg.style.visibility="visible";
+                                                msg.innerHTML="Happy Independence Day!!"
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    
